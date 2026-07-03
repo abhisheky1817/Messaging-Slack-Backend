ec7e59e5f3959bd72566f7563c0568b7a124be55
abhisheky1817 <abhisheky.1817@gmail.com>
2026-07-03 16:59:39 +0530

    added signup with zod validation

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { signUp } from '../../controllers/userController.js';
+import { userSignUpSchema } from '../../validators/userSchema.js';
+import { validate } from '../../validators/zodValidator.js';
 
 const router = express.Router();
 
-router.post('/signup', signUp);
+router.post('/signup', validate(userSignUpSchema), signUp);
 
 export default router;
