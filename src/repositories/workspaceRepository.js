ad74f21a9aed96931d32f63af70178dc53991420
abhisheky1817 <abhisheky.1817@gmail.com>
2026-07-06 11:14:58 +0530

    added channelrepo and workspacerepo

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -90,6 +90,48 @@ const  workspaceRepository = {
     return workspace;
   },
 
+   addChannelToWorkspace: async function (workspaceId, channelName) {
+    const workspace =
+      await Workspace.findById(workspaceId).populate('channels');
+
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isChannelAlreadyPartOfWorkspace = workspace.channels.find(
+      (channel) => channel.name === channelName
+    );
+
+    if (isChannelAlreadyPartOfWorkspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from client',
+        message: 'Channel already part of workspace',
+        statusCode: StatusCodes.FORBIDDEN
+      });
+    }
+
+    const channel = await channelRepository.create({
+      name: channelName,
+      workspaceId: workspaceId
+    });
+
+    workspace.channels.push(channel);
+    await workspace.save();
+
+    return workspace;
+  },
+  
+  fetchAllWorkspaceByMemberId: async function (memberId) {
+    const workspaces = await Workspace.find({
+      'members.memberId': memberId
+    }).populate('members.memberId', 'username email avatar');
+
+    return workspaces;
+  }
  
 };
 
