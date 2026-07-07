7ea3daf0ec9ae73a97926012ba4857727c337f7d
abhisheky1817 <abhisheky.1817@gmail.com>
2026-07-07 10:18:32 +0530

    fix bug

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import jwt from 'jsonwebtoken';
 
-import { JWT_SECRET } from '../config/serverConfig.js';
+import { JWT_SECRET } from '../config/serverconfig.js';
 import userRepository from '../repositories/userRepository.js';
 import {
   customErrorResponse,
