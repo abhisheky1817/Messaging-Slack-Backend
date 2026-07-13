326c03e4bbf875fac3b06fb5c1d9e396d459c8f3
abhisheky1817 <abhisheky.1817@gmail.com>
2026-07-13 12:49:13 +0530

    added member api to check user part of workspace

--- src/routes/v1/v1Router.js
+++ src/routes/v1/v1Router.js
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './user.js';
 import workspaceRouter from './workspaces.js';
 import channelRouter from './channel.js';
+import memberRouter from './member.js';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/workspaces', workspaceRouter);
 router.use('/channel',channelRouter);
+router.use('/members', memberRouter);
 
 export default router;
