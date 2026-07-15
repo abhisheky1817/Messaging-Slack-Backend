12b52d8648428a25dc963fd4e4819e1127043180
abhisheky1817 <abhisheky.1817@gmail.com>
2026-07-15 15:34:41 +0530

    added messages in channel response

--- src/services/channelService.js
+++ src/services/channelService.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
 import channelRepository from '../repositories/channelRepository.js';
+import messageRepository from '../repositories/messageRepository.js';
 import ClientError from '../utils/errors/clientError.js';
 import { isUserMemberOfWorkspace } from './workspaceService.js';
 
@@ -31,9 +32,24 @@ export const getChannelByIdService = async (channelId, userId) => {
       });
     }
 
+     const messages = await messageRepository.getPaginatedMessaged(
+      {
+        channelId
+      },
+      1,
+      20
+    );
+
     console.log('Channel in service', channel);
 
-   return channel;
+    return {
+      messages,
+      _id: channel._id,
+      name: channel.name,
+      createdAt: channel.createdAt,
+      updatedAt: channel.updatedAt,
+      workspaceId: channel.workspaceId
+    };
 
   } catch (error) {
     console.log('Get channel by ID service error', error);
